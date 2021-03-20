9103ac6f2b76ff8e4b75908fd6e636d498d57a5a
Clivern <hello@clivern.com>
2021-03-20 22:29:10 +0100

    int

--- core/runtime/docker.go
+++ core/runtime/docker.go
@@ -1,3 +1,15 @@
 // Copyright 2021 Clivern. All rights reserved.
 // Use of this source code is governed by the MIT
 // license that can be found in the LICENSE file.
+
+package runtime
+
+// Docker type
+type Docker struct {
+}
+
+// NewDocker creates a new instance
+func NewDocker() *Docker {
+	instance := new(Docker)
+	return instance
+}
